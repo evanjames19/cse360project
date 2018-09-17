fa4cf9a76c29667efb075326c0f8f160381952e2
Evan Weiss <ejweiss2@asu.edu>
2018-09-17 12:39:02 -0700

    Add files via upload

---
+++ cse360project/src/src/main.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+public class main
+{
+	
+	
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setSize(500,500);
+    JButton button = new JButton("Press");
+    frame.getContentPane().add(button); // Adds Button to content pane of frame
+    frame.setVisible(true);
+    
+}
+}
